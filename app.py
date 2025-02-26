8afdc3ab76cfe07465baefedbfdc10b7cdf83e99
XinChengLin <xclin@wondercore.com>
2025-02-26 21:40:51 +0800

    style:刪除註解

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
 from flask import Flask, request, abort
-#from linebot import LineBotApi, WebhookHandler
-#from linebot.exceptions import InvalidSignatureError
-#from linebot.models import MessageEvent, TextMessage, TextSendMessage
-#import os
-#import openai
 from config import OPENAI_API_KEY, LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET
 from line_bot import handle_line_event  # 匯入處理訊息的方法
 
 
 app = Flask(__name__)
-
-#line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-#handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-# 設定 OpenAI API Key
-#openai.api_key = OPENAI_API_KEY
-
-#client = openai.OpenAI()  # 創建 OpenAI 客戶端
 
 @app.route("/")
 def home():
@@ -35,26 +22,5 @@ def callback():
 
     return 'OK'
 
-"""
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_message = event.message.text
-    
-
-
-    # 呼叫 OpenAI API
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",  # 或 "gpt-4"（需確認你的 API Key 是否有權限）
-        messages=[{"role": "user", "content": user_message}]
-    )
-
-    chatgpt_reply = response.choices[0].message.content.strip()
-
-    # 回應使用者
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=chatgpt_reply)
-    )
-"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
